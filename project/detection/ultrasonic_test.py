101b06d51e77de507034cea2528945ec738bf7ab
jboulte-stack <jboulte@g.clemson.edu>
2026-03-30 16:19:19 -0400

    ultrasonic test script

---
+++ project/detection/ultrasonic_test.py
@@ -0,0 +1,33 @@
+
+#!/usr/bin/env python3
+
+import sys
+import time
+
+sys.path.append('/home/pi/project_demo/lib')
+from Raspbot_Lib import Raspbot
+
+bot = Raspbot()
+
+def main():
+    bot.Ctrl_Ulatist_Switch(1) # Enable ultrasonic sensor
+
+    while True:
+        time.sleep(0.5) # sensor delay
+        # Read distance from sensor
+        dist_H =bot.read_data_array(0x1b,1)[0]
+        dist_L =bot.read_data_array(0x1a,1)[0]
+        dist = dist_H << 8 | dist_L 
+        print(f"Distance: {dist} cm")
+        if dist < 15: # If an obstacle is detected within 15 cm
+            bot.Ctrl_WQ2812_ALL(1,0)
+        elif dist < 30: # If an obstacle is detected within 30 cm
+            bot.Ctrl_WQ2812_ALL(1,1)
+        else: # If an obstacle is not detected within 30 cm
+            bot.Ctrl_WQ2812_ALL(1,3)
+
+
+
+if __name__ == "__main__":
+    main()
+
