a04a895bc220daf226f6f1e6c608306bc5fbbdcd
jboulte-stack <jboulte@g.clemson.edu>
2026-04-06 17:38:06 -0400

    update calibration

--- project/detection/dr_calibration.py
+++ project/detection/dr_calibration.py
@@ -138,8 +138,8 @@ def test_forward(robot):
     try:
         robot.Ctrl_Muto(0, APPROACH_SPEED) ## Left Front
         robot.Ctrl_Muto(1, APPROACH_SPEED) ## Left Rear
-        robot.Ctrl_Muto(2, APPROACH_SPEED*1.05) ## Right Front
-        robot.Ctrl_Muto(3, APPROACH_SPEED*1.05) ## Right Rear
+        robot.Ctrl_Muto(2, APPROACH_SPEED*1.1) ## Right Front
+        robot.Ctrl_Muto(3, APPROACH_SPEED*1.1) ## Right Rear
         time.sleep(TEST_DURATION)
     finally:
         robot.Ctrl_Car(0, 0, 0)
