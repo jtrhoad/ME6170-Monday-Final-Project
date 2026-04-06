ae7e942526512c7d75dadf0866856a2fa8c7e998
jboulte-stack <jboulte@g.clemson.edu>
2026-04-06 17:50:07 -0400

    update calibration

--- project/detection/dr_calibration.py
+++ project/detection/dr_calibration.py
@@ -134,8 +134,9 @@ def test_forward(robot):
 
     wait_for_enter('Position the bot on a clear straight path and press ENTER...')
     countdown(3, 'Forward movement test starting...')
-    APPROACH_SPEED_R = 60  # Compensate for veering right
-
+    APPROACH_SPEED_R = APPROACH_SPEED * 1.2  # Compensate for veering right
+    print(APPROACH_SPEED_R)
+    
     try:
         robot.Ctrl_Muto(2, APPROACH_SPEED_R) ## Right Front
         robot.Ctrl_Muto(3, APPROACH_SPEED_R) ## Right Rear
