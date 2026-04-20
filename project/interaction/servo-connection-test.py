b433dcf3ccbdbe6260a506ccbd66b7bd7048b885
jt-redd <jtapp.rhoads@gmail.com>
2026-04-20 16:05:48 -0400

    Updated servo test

--- project/interaction/servo-connection-test.py
+++ project/interaction/servo-connection-test.py
@@ -1,43 +1,36 @@
-import pigpio
-import time
- 
-# Connect to pigpio daemon
-pi = pigpio.pi()
- 
-if not pi.connected:
-    print("Failed to connect to pigpio daemon")
-    exit()
- 
-# Define GPIO pins
-SERVO1 = 17
-SERVO2 = 7
- 
-# Function to move servo (pulse width in microseconds)
-def set_servo(pin, pulse):
-    pi.set_servo_pulsewidth(pin, pulse)
- 
+#!/usr/bin/env python3
+from gpiozero import AngularServo
+from gpiozero.pins.lgpio import LGPIOFactory
+from time import sleep
+
+# Force lgpio backend (required for Pi 5)
+factory = LGPIOFactory()
+
+servo1 = AngularServo(18, min_angle=0, max_angle=180,
+                      min_pulse_width=0.0005, max_pulse_width=0.0025,
+                      pin_factory=factory)
+servo2 = AngularServo(19, min_angle=0, max_angle=180,
+                      min_pulse_width=0.0005, max_pulse_width=0.0025,
+                      pin_factory=factory)
+
 try:
     while True:
-        # Move to minimum (~0 degrees)
-        set_servo(SERVO1, 500)
-        set_servo(SERVO2, 500)
-        time.sleep(1)
- 
-        # Move to middle (~90 degrees)
-        set_servo(SERVO1, 1500)
-        set_servo(SERVO2, 1500)
-        time.sleep(1)
- 
-        # Move to maximum (~180 degrees)
-        # set_servo(SERVO1, 2500)
-        # set_servo(SERVO2, 2500)
-        # time.sleep(1)
- 
+        print("0 degrees")
+        servo1.angle = 0
+        servo2.angle = 0
+        sleep(1)
+
+        print("90 degrees")
+        servo1.angle = 90
+        servo2.angle = 90
+        sleep(1)
+
+        print("180 degrees")
+        servo1.angle = 180
+        servo2.angle = 180
+        sleep(1)
+
 except KeyboardInterrupt:
-    pass
- 
-# Stop servos
-set_servo(SERVO1, 0)
-set_servo(SERVO2, 0)
- 
-pi.stop()
+    servo1.detach()
+    servo2.detach()
+    print("Done.")
